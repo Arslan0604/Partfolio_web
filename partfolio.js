6521d0d8c613a84982b971fb70efe37479e86d93
Arslan Ovezov <arslan.datascience@gmail.com>
2025-01-12 13:52:32 +0500

    finaly start

---
+++ partfolio.js
@@ -0,0 +1,8 @@
+// Contact Form Submission
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent default form submission
+  
+    // Simulate form submission (replace with actual backend code)
+    alert('Message sent successfully!');
+    this.reset(); // Clear the form
+  });
